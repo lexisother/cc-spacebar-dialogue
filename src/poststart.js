6d46d5308510a1754b552a1b33d109e75f6701cc
Alyxia Sother <alyxia@riseup.net>
2026-05-16 11:44:28 +0200

    [Meta] Initial commit

---
+++ src/poststart.js
@@ -0,0 +1,12 @@
+sc.ScreenInteractEntry.inject({
+  update() {
+    if (this.callbackObject instanceof sc.MessageModel) {
+      if (ig.input.pressed('special')) {
+        ig.interact.setBlockDelay();
+        this.callbackObject.onInteraction();
+      }
+    } else {
+      this.parent();
+    }
+  },
+});
